e55d961cad1d84444835b62c4aba5d704cdfdb0a
Abhijeet2010 <abhijeetraut220@gmail.com>
2023-02-11 15:40:41 +0530

    completed react blogreact

--- src/Components/Latest.jsx
+++ src/Components/Latest.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import LatestCard from './LatestCard'
+import React from "react";
+import LatestCard from "./LatestCard";
 
 const Latest = (props) => {
   return (
     <>
-    <div className="latestContainer">
-    <h1 style={{ borderBottom : "0.2rem solid black", width : "10rem" }} >{ props.value.latest } </h1>
-    <LatestCard value={props.value.latestArr} ></LatestCard>
-    </div>
+      <div className="latestContainer">
+        <h1 style={{ borderBottom: "0.2rem solid black", width: "10rem" }}>
+          {props.value.latest}
+        </h1>
+        <LatestCard value={props.value.latestArr}></LatestCard>
+      </div>
     </>
-  )
-}
+  );
+};
 
-
-export default Latest
+export default Latest;
